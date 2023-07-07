8dfb2c449d0ef89ad758cd2b9e145c4b59fa4f10
yellowlemon22 <yellowlemon1106@gaml.com>
2023-07-07 21:52:16 +0900

    一時保存

--- js/detail.js
+++ js/detail.js
@@ -1,61 +1,221 @@
 const py = [
   {
+    id: "pythonContent",
+    class: "content active",
     code: "Python",
     explanation: "１個目の記事だよー"
   },
   {
+    id: "pythonContent",
+    class: "content active",
     code: "Python",
     explanation: "２個目の記事だよー"
   },
   {
+    id: "pythonContent",
+    class: "content active",
     code: "Python",
-    explanation: "３個目の記事だよ３"
+    explanation: "３個目の記事だよー"
   },
   {
+    id: "pythonContent",
+    class: "content active",
     code: "Python",
     explanation: "４個目の記事だよー"
   },
   {
+    id: "pythonContent",
+    class: "content active",
     code: "Python",
     explanation: "５個目の記事だよー"
   },
   {
+    id: "pythonContent",
+    class: "content active",
     code: "Python",
     explanation: "６個目の記事だよー"
   },
   {
+    id: "pythonContent",
+    class: "content active",
     code: "Python",
     explanation: "7個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "8個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "9個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "10個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "11個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "12個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "13個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "14個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "15個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "16個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "17個目の記事だよー"
+  },
+  {
+    id: "pythonContent",
+    class: "content active",
+    code: "Python",
+    explanation: "18個目の記事だよー"
   },
 ]
 
 const js = [
   {
+    id: "javascriptContent",
+    class: "content active",
     code: "JavaScript",
-    explanation: "１個目の記事だよー"
+    explanation: "１個目の記事だよ"
   },
   {
+    id: "javascriptContent",
+    class: "content active",
     code: "JavaScript",
-    explanation: "２個目の記事だよー"
+    explanation: "２個目の記事だよ"
   },
   {
+    id: "javascriptContent",
+    class: "content active",
     code: "JavaScript",
-    explanation: "３個目の記事だよ３"
+    explanation: "３個目の記事だよ"
   },
   {
+    id: "javascriptContent",
+    class: "content active",
     code: "JavaScript",
-    explanation: "４個目の記事だよー"
+    explanation: "４個目の記事だよ"
   },
   {
+    id: "javascriptContent",
+    class: "content active",
     code: "JavaScript",
-    explanation: "５個目の記事だよー"
+    explanation: "５個目の記事だよ"
   },
   {
+    id: "javascriptContent",
+    class: "content active",
     code: "JavaScript",
-    explanation: "６個目の記事だよー"
+    explanation: "６個目の記事だよ"
   },
   {
+    id: "javascriptContent",
+    class: "content active",
     code: "JavaScript",
-    explanation: "7個目の記事だよー"
+    explanation: "7個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "8個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "9個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "10個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "11個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "12個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "13個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "14個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "15個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "16個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "17個目の記事だよ"
+  },
+  {
+    id: "javascriptContent",
+    class: "content active",
+    code: "JavaScript",
+    explanation: "18個目の記事だよ"
   },
 ]
